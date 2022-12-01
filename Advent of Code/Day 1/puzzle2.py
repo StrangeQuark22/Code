1b06688bbb85f38b5b3c761463abfc3a83dfdafe
StrangeQuark22 <marcell.ziegler.22@gmail.com>
2022-12-01 20:00:23 +0100

    day 1

---
+++ Advent of Code/Day 1/puzzle2.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+allData = ""
+with open("./Day 1/input1.txt", "r", encoding="utf-8") as file:
+    for line in file:
+        allData += line
+
+elfs = np.array(
+    [np.array(bundle.split("\n")).astype(int) for bundle in allData.split("\n\n")]
+)
+print(elfs)
+
+sums = []
+
+for elf in elfs:
+    sums.append(sum(elf))
+print("\n\n largest: ", max(sums))
